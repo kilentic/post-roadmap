fcd937dc8668d7ce7cd3c81db9fc5dd60045cc33
dai <dainguyenblabla@gmail.com>
2020-08-02 17:44:29 +0700

    delete pg

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-uri = URI.parse ENV['REDIS_URI'] 
-Redis.current = Redis.new uri: uri
-#Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+#uri = URI.parse ENV['REDIS_URI'] 
+#Redis.current = Redis.new uri: uri
+Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
