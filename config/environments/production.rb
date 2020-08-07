4b44b0fd82e470a9e0f6a01bf938660deeaad499
Nguyễn Đức Đại <36080683+dainguyenbla@users.noreply.github.com>
2020-08-07 23:15:56 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  Rails.application.routes.default_url_options[:host] = 'ec2-18-207-142-182.compute-1.amazonaws.com'
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
